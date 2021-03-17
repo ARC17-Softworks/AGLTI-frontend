51b4442afe9ed6fd16542877bdd65802ea4b1288
Bilal-Z <bilalzubairi@hotmail.com>
2021-03-17 19:59:28 +0500

    fix login query

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -1,11 +1,15 @@
-import { gql } from 'apollo-boost';  //for get
+import { gql } from 'apollo-boost'; //for get
 
 const LOGIN_USER = gql`
-  mutation($email: String!, $password: String!) {
-    login(input: {email: $email, password: $password}) {
-        name
-    }
-  }
-`
+	mutation($email: String!, $password: String!) {
+		login(input: { email: $email, password: $password }) {
+			user {
+				id
+				email
+				name
+			}
+		}
+	}
+`;
 
 export { LOGIN_USER };
