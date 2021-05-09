b5359341430c0ea657a1af5a9c725ce34c9072b5
Muhammad_Bilal_Rizwan <mb92733@gmail.com>
2021-05-09 15:56:51 +0500

    ProjectMember Hooks

--- src/components/ProjectMembersPage/ProjectMembers.js
+++ src/components/ProjectMembersPage/ProjectMembers.js
@@ -1,48 +1,91 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, SimpleGrid, Stack, Icon, Heading, Grid, Avatar, Text, Divider, Tag, Button } from '@chakra-ui/core';
 import ProjectMemberCard from './ProjectMemberCard';
 
-export class ProjectMembersSection extends Component {
-    render() {
-        return (
-            <Box zIndex={-1} h="100vh" bg="#171923" color="white">
-                <SimpleGrid columns={0} spacing={0} ml="8%">
-                <Box h="38vh" w="85vw" mt="100px" borderRadius="lg" bg="#2D3748" >
-                     <Heading size="xl" textAlign="left" ml="102px" >Members</Heading>
-                     <Stack isInline>
-                        <Icon name="chevron-left" size="80px" mt="40px" ml="10px"/>
-                        
-                        <Grid templateColumns="repeat(4, 1fr)" gap={6} ml="1px" mr="8%" mt="1%">
-                            <ProjectMemberCard />
-                            <ProjectMemberCard />
-                            <ProjectMemberCard />
-                            <ProjectMemberCard />
-                        </Grid>
+function ProjectMembersSection() {
+    let projectMembers = [
+        {
+            id: 1,
+            name: "Bilal Rizwan",
+            position: "Project Manager"
+        },
+        {
+            id: 2,
+            name: "Anas Zafar",
+            position: "Frontend Developer"
+        },
+        {
+            id: 3,
+            name: "Bilal Zubairi",
+            position: "Backend Developer"
+        }
+      ];
+    let prevMembers = [
+        {
+            id: 1,
+            name: "Bilal Rizwan",
+            position: "Frontend Developer"
+        },
+        {
+            id: 2,
+            name: "Anas Zafar",
+            position: "UI/UX"
+        },
+        {
+            id: 3,
+            name: "Bilal Zubairi",
+            position: "Backend Developer"
+        }
+      ];
 
-                        <Icon name="chevron-right" size="80px" mt="40px" ml="10px" mr="10px"/>
-                     </Stack>
-                 </Box>
+    const [members, setMember] = useState([]);
+    const [previousMembers, setPreviousMember] = useState([]);
+
+    useEffect(() => {
+        setMember(projectMembers);
+        setPreviousMember(prevMembers);
+      },[]);
+        
+    console.log(members);
+    console.log(previousMembers);
+    return (
+        <Box zIndex={-1} h="100vh" bg="#171923" color="white">
+            <SimpleGrid columns={0} spacing={0} ml="8%">
+            <Box h="38vh" w="85vw" mt="100px" borderRadius="lg" bg="#2D3748" >
+                <Heading size="xl" textAlign="left" ml="102px" >Members</Heading>
+                <Stack isInline>
+                    <Icon name="chevron-left" size="80px" mt="40px" ml="10px"/>
+                    {members.map((member,id) => {
+                        return(
+                            <Grid templateColumns="repeat(4, 1fr)" gap={6} ml="1px" mr="8%" mt="1%">
+                                <ProjectMemberCard  key={id} member={member}/>
+                            </Grid>
+                        );
+                    })}
+                    <Icon name="chevron-right" size="80px" mt="40px" ml="10px" mr="10px"/>
+                </Stack>
+            </Box>
 
                  
-                 <Box h="38vh" w="85vw" mt="30px" borderRadius="lg" bg="#2D3748" >
-                 <Heading size="xl" textAlign="left" ml="102px" >Previous Members</Heading>
-                     <Stack isInline>
-                        <Icon name="chevron-left" size="80px" mt="40px" ml="10px"/>
+            <Box h="38vh" w="85vw" mt="30px" borderRadius="lg" bg="#2D3748" >
+                <Heading size="xl" textAlign="left" ml="102px" >Previous Members</Heading>
+                <Stack isInline>
+                    <Icon name="chevron-left" size="80px" mt="40px" ml="10px"/>
+                    {previousMembers.map((member,id) => {
+                        return(
+                            <Grid templateColumns="repeat(4, 1fr)" gap={6} ml="1px" mr="8%" mt="1%">
+                                <ProjectMemberCard  key={id} member={member}/>
+                            </Grid>
+                        );
+                    })}
+                    
 
-                        <Grid templateColumns="repeat(4, 1fr)" gap={6} ml="1px" mr="8%" mt="1%">
-                            <ProjectMemberCard />
-                            <ProjectMemberCard />
-                            <ProjectMemberCard />
-                            <ProjectMemberCard />
-                        </Grid>
-
-                        <Icon name="chevron-right" size="80px" mt="40px" ml="1px"/>
-                     </Stack>
-                 </Box>
-                </SimpleGrid>
+                    <Icon name="chevron-right" size="80px" mt="40px" ml="1px"/>
+                </Stack>
             </Box>
-        )
-    }
+            </SimpleGrid>
+        </Box>
+)
 }
 
 export default ProjectMembersSection
