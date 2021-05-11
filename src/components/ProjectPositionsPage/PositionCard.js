95331be0b722be46694b58fb8f3b91e83743e2b3
Muhammad_Bilal_Rizwan <mb92733@gmail.com>
2021-05-11 05:11:34 +0500

    Position Card Hooks

--- src/components/ProjectPositionsPage/PositionCard.js
+++ src/components/ProjectPositionsPage/PositionCard.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import { Box, Grid, SimpleGrid, Stack, Heading, Icon, Text, Button, Tag, CloseButton, Badge, Avatar, AvatarBadge, Divider } from "@chakra-ui/core";
+import { Box, Grid, SimpleGrid, Stack,  Heading, Icon, Text, Button, Tag, CloseButton, Badge, Avatar, AvatarBadge, Divider } from "@chakra-ui/core";
 
-class PositionCard extends Component {
-    render() {
+function PositionCard(props) {
+    let skillArr = props.position.skills;
         return (
             <Stack direction="row" spacing={-5} mt="auto">
                         <Stack spacing={0}>
                             <Box w="120%" h="37vh" bg="#4A5568" roundedTop="lg">
                                 <Heading textAlign="center" mt="1%" as="h6" size="s">
-                                    Position Title
+                                    {props.position.title}
                                 </Heading>
                                 <Text ml="8%" mt="2%" fontSize="xs">
-                                Lorem ipsum is placeholder text commonly used in the graphic, print, and
-                                publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and
-                                publishing industries for previewing layouts and visual mockups.
+                                {props.position.description}
                                 </Text>
-                                <Stack isInline spacing={2} ml="10px" mr="10%"  >
-                                    <Tag size="xs" bg="#718096" borderRadius="lg" color="white">HTML</Tag>
-                                    <Tag size="xs" bg="#718096" borderRadius="lg" color="white">CSS</Tag>
-                                    <Tag size="xs" bg="#718096" borderRadius="lg" color="white">Javascript</Tag>
-                                </Stack>
+                                {skillArr.map((skill) => {
+                                return(
+                                    <Tag size="xs" bg="#718096" ml="3px" mr="3px" borderRadius="lg" color="white">{skill}</Tag>
+                                );
+                            }
+                            )}
                             </Box>
                             
                             <Button w="120%" h="5vh" bg="#718096" roundedTop="0" roundedBottom="lg" variant="solid">
@@ -28,7 +27,6 @@ class PositionCard extends Component {
                         </Stack>
                         </Stack>
         )
-    }
 }
 
 export default PositionCard;
--- src/components/ProjectPositionsPage/ProjectPositions.js
+++ src/components/ProjectPositionsPage/ProjectPositions.js
@@ -1,67 +1,173 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Box, Grid, SimpleGrid, Stack, Heading, Icon, Text, Button, Tag, CloseButton, Badge, Avatar, AvatarBadge, Divider } from "@chakra-ui/core";
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import PositionCard from './PositionCard';
 import OfferCard from './OfferCard';
 import ApplicantCard from './ApplicantCard';
 
-class ProjectPositionsSection extends Component {
-    render() {
-        return (
-            <Box zIndex={-1} bg="#171923" h="100%" color="white" >
-                <SimpleGrid columns={1} spacing={4} ml="8%">
-                 <Box h="50vh" w="85vw" mt="6%" borderRadius="lg" bg="#2D3748" >
-                    <Stack isInline spacing="auto" ml="102px" mr="40px">
-                        <Heading textAlign="left" size="xl">Positions</Heading>
-                            <Stack isInline spacing="auto" ml="8%" mr="40px" mt="12px">
+function ProjectPositionsSection() {
+
+    const skills = [0];
+    const [positions,setPositions] = useState([
+        {
+          id: 1,
+          title: "Title of Position",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere dui vel urna egestas rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere dui vel urna egestas rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere dui vel urna egestas rutrum. ",
+          skills : ["Javascript", "Reactjs", "CSS"]
+        },
+        {
+            id: 2,
+            title: "Title of Discussion",
+            description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere dui vel urna egestas rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere dui vel urna egestas rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere dui vel urna egestas rutrum. ",
+            skills : ["Javascript", "Reactjs", "CSS"]
+        },
+        {
+            id: 3,
+            title: "Title of Discussion",
+            description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere dui vel urna egestas rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere dui vel urna egestas rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere dui vel urna egestas rutrum. ",
+            skills : ["Javascript", "Reactjs", "CSS"]
+        }
+      ]);
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleAdd = () => {
+        let obj1 = {title: document.getElementById("title").value,
+                    description: document.getElementById("description").value,
+                    skills: document.getElementById("skills").value.split(',')}
+        let obj2 = positions;
+        obj2.push(obj1)
+        setPositions(obj2);
+        handleClose();
+    }
+
+    return (
+        <Box zIndex={-1} bg="#171923" h="100%" color="white" >
+            <SimpleGrid columns={1} spacing={4} ml="8%">
+            <Box h="50vh" w="85vw" mt="6%" borderRadius="lg" bg="#2D3748" >
+                <Stack isInline spacing="auto" ml="102px" mr="40px">
+                    <Heading textAlign="left" size="xl">Positions</Heading>
+                        <Button ml="8%" mr="40px" mt="1px" color="black" onClick={handleClickOpen}>
+                            <Stack isInline spacing="auto">
                                 <Icon name="small-add" size="18px" />
                                 <Heading textAlign="left" size="sm">Add Position</Heading>
                             </Stack>
-                    </Stack>
-                    <Stack isInline spacing="auto" ml="10px" mr="40px" >
-                        <Icon name="chevron-left" size="80px" mt="80px" ml="10px"/>
-                    <Grid templateColumns="repeat(4, 1fr)" gap={16} ml="6px">
-                        <PositionCard />
-                        <PositionCard />
-                        <PositionCard />
-                        <PositionCard />
-                    </Grid>
+                        </Button>
+                </Stack>
+                <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Add Position</DialogTitle>
+                <DialogContent>
+                <TextField
+                        autoFocus
+                        margin="dense"
+                        id="title"
+                        label="Position Title"
+                        type="text"
+                        fullWidth
+                        // value={}
+                        // onChange={(e)=>handleChange(e)}
+                        // onChange={handleChange}
+                        // onChange={(e)=>setTitle(e.target.value)}
+                        // onChange={handleChange}    
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="description"
+                        label="Position Description"
+                        type="text"
+                        fullWidth
+                        // value={}
+                        // onChange={(e)=>handleChange(e)}
+                        // onChange={handleChange}
+                        // onChange={(e)=>setTitle(e.target.value)}
+                        // onChange={handleChange}    
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="skills"
+                        label="Skills"
+                        type="text"
+                        fullWidth
+                        // value={}
+                        // onChange={(e)=>handleChange(e)}
+                        // onChange={this.handleChange}
+                        // onChange={(e)=>setDescription(e.target.value)}
+                        //   onChange={handleChange}    
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                    Cancel
+                    </Button>
+                    <Button onClick={handleAdd} color="primary" type="submit">
+                    Add
+                    </Button>
+                </DialogActions>
+            </Dialog>
+                <Stack isInline spacing="auto" ml="10px" mr="40px" >
+                    <Icon name="chevron-left" size="80px" mt="80px" ml="10px"/>
+                    {positions.map((position, id) => {
+                        return(
+                            <Grid templateColumns="repeat(4, 1fr)" gap={4} ml="6px">
+                                <PositionCard key={id} position={position}/>
+                            </Grid>
+                        );
+                    }
+                    )}
                     <Icon name="chevron-right" size="80px" ml="40px" mt="80px" mr="20px"/>
                 </Stack>
-                 </Box>
+            </Box>
 
 
-                <Box h="50vh" w="85vw" borderRadius="lg" bg="#2D3748" >
-                    <Heading textAlign="left" size="xl" ml="8%">Offered</Heading>
-                    <Stack isInline spacing="auto" ml="10px" mr="40px" >
-                        <Icon name="chevron-left" size="80px" mt="80px" ml="10px"/>
-                        <Grid templateColumns="repeat(4, 1fr)" gap={6} ml="-120px" mr="8%" mt="1%">
-                            <OfferCard />
-                            <OfferCard />
-                            <OfferCard />
-                            <OfferCard />
-                        </Grid>
-                        <Icon name="chevron-right" size="80px" ml="90px" mt="80px" mr="20px"/>
-                    </Stack>
-                </Box>
+            <Box h="50vh" w="85vw" borderRadius="lg" bg="#2D3748" >
+                <Heading textAlign="left" size="xl" ml="8%">Offered</Heading>
+                <Stack isInline spacing="auto" ml="10px" mr="40px" >
+                    <Icon name="chevron-left" size="80px" mt="80px" ml="10px"/>
+                    <Grid templateColumns="repeat(4, 1fr)" gap={4} ml="-120px" mr="8%" mt="1%">
+                        <OfferCard />
+                        <OfferCard />
+                        <OfferCard />
+                        <OfferCard />
+                    </Grid>
+                    <Icon name="chevron-right" size="80px" ml="90px" mt="80px" mr="20px"/>
+                </Stack>
+            </Box>
 
                 
-                <Box h="50vh" w="85vw" borderRadius="lg" bg="#2D3748" >
-                    <Heading textAlign="left" size="xl" ml="8%">Applicants</Heading>
-                    <Stack isInline spacing="auto" ml="10px" mr="40px" >
-                        <Icon name="chevron-left" size="80px" mt="80px" ml="10px"/>
-                        <Grid templateColumns="repeat(4, 1fr)" gap={6} ml="-120px" mr="8%" mt="1%">
-                            <ApplicantCard />
-                            <ApplicantCard />
-                            <ApplicantCard />
-                            <ApplicantCard />
-                        </Grid>
-                        <Icon name="chevron-right" size="80px" ml="90px" mt="80px" mr="20px"/>
-                    </Stack>
-                 </Box>
-                </SimpleGrid>
+            <Box h="50vh" w="85vw" borderRadius="lg" bg="#2D3748" >
+                <Heading textAlign="left" size="xl" ml="8%">Applicants</Heading>
+                <Stack isInline spacing="auto" ml="10px" mr="40px" >
+                    <Icon name="chevron-left" size="80px" mt="80px" ml="10px"/>
+                    <Grid templateColumns="repeat(4, 1fr)" gap={6} ml="-120px" mr="8%" mt="1%">
+                        <ApplicantCard />
+                        <ApplicantCard />
+                        <ApplicantCard />
+                        <ApplicantCard />
+                    </Grid>
+                    <Icon name="chevron-right" size="80px" ml="90px" mt="80px" mr="20px"/>
+                </Stack>
             </Box>
+            </SimpleGrid>
+        </Box>
         )
-    }
 }
 
 export default ProjectPositionsSection;
