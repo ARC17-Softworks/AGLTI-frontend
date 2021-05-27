497b1a91dfb2ee91f5ebefc72231d21fd9bf1c4e
Bilal <bilalzubairi@hotmail.com>
2021-05-27 23:32:17 +0500

    add create project page

--- src/components/project/MemberCard.jsx
+++ src/components/project/MemberCard.jsx
@@ -19,14 +19,14 @@ export const MemberCard = ({ member }) => {
       <LinkBox
         maxW={'320px'}
         w={'full'}
-        bg={useColorModeValue('gray.50', 'gray.700')}
+        bg={useColorModeValue('white', 'gray.700')}
         boxShadow={'2xl'}
         rounded={'lg'}
         p={6}
         textAlign={'center'}
       >
         <Avatar size={'xl'} src={member.dev.avatar} mb={4} />
-        <LinkOverlay as={RouterLink} isExternal href={`/user/${member.dev.id}`}>
+        <LinkOverlay as={RouterLink} isExternal to={`/user/${member.dev.id}`}>
           <Heading fontSize={'2xl'}>{member.dev.name}</Heading>
         </LinkOverlay>
         <Text fontWeight={600} color={'gray.500'} mb={4}>
