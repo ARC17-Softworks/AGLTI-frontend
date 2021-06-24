bf1dc71ff5f5a0b811dc0997742cf11ead0b7afe
Bilal <bilalzubairi@hotmail.com>
2021-06-24 11:52:45 +0500

    Project Dashboard fix layout
    remove box wrapping routes and give each route own box

--- src/components/project/InfoArea.jsx
+++ src/components/project/InfoArea.jsx
@@ -203,138 +203,148 @@ export const InfoArea = props => {
   }
 
   return (
-    <Box w="full" p={'6'} borderWidth="1px" rounded="md">
-      <Flex w="full">
-        <Heading>{project.title}</Heading>
-        <Spacer />
-        {authContext.profile.projectOwner ? (
-          <Tooltip hasArrow label="Edit Project">
-            <IconButton
-              onClick={onEditOpen}
-              icon={<EditIcon />}
-              variant="outline"
-            />
-          </Tooltip>
-        ) : (
-          <Box></Box>
-        )}
-        <Modal
-          isOpen={isEditOpen}
-          onClose={onEditClose}
-          size="3xl"
-          scrollBehavior="inside"
-        >
-          <ModalOverlay />
-          <ModalContent>
-            <ModalCloseButton />
-            <ModalBody>
-              <chakra.form onSubmit={onSubmit} {...props}>
-                <Stack spacing="6">
-                  <Heading textAlign="center" size="xl" fontWeight="extrabold">
-                    Update Project
-                  </Heading>
-                  <FormControl id="title">
-                    <FormLabel>Title</FormLabel>
-                    <Input
-                      name="title"
-                      type="text"
-                      value={values.title}
-                      onChange={onChange}
-                      required
-                    />
-                  </FormControl>
-                  <FormControl id="description">
-                    <FormLabel>Description</FormLabel>
-                    <Textarea
-                      name="description"
-                      value={values.description}
-                      onChange={onChange}
-                    />
-                  </FormControl>
-                  <Button
-                    type="submit"
-                    colorScheme="green"
-                    size="lg"
-                    fontSize="md"
-                    isLoading={editLoading}
-                    loadingText="Updating..."
-                    spinnerPlacement="end"
-                  >
-                    Update Project
-                  </Button>
-                </Stack>
-              </chakra.form>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
-      </Flex>
-      <Text mt={6} py={2} fontSize="xl" fontWeight="black">
-        description:
-      </Text>
-      <Text fontStyle="italic">"{project.description}"</Text>
-      <Divider py={3} />
-      <Flex pt={1} alignItems="end" w="full" justify="space-between">
-        <VStack alignItems="start">
-          <Text fontStyle="italic" fontWeight="bold">
-            created by:
-          </Text>
-          <VStack justifyContent="center">
-            <Link isExternal as={RouterLink} to={`/user/${project.owner.id}`}>
-              <Avatar size="xl" src={project.owner.avatar} mb={1} />
-            </Link>
+    <Box maxW="container.xl" px={10} pt={2} mx="auto">
+      <Box w="full" p={'6'} borderWidth="1px" rounded="md">
+        <Flex w="full">
+          <Heading>{project.title}</Heading>
+          <Spacer />
+          {authContext.profile.projectOwner ? (
+            <Tooltip hasArrow label="Edit Project">
+              <IconButton
+                onClick={onEditOpen}
+                icon={<EditIcon />}
+                variant="outline"
+              />
+            </Tooltip>
+          ) : (
+            <Box></Box>
+          )}
+          <Modal
+            isOpen={isEditOpen}
+            onClose={onEditClose}
+            size="3xl"
+            scrollBehavior="inside"
+          >
+            <ModalOverlay />
+            <ModalContent>
+              <ModalCloseButton />
+              <ModalBody>
+                <chakra.form onSubmit={onSubmit} {...props}>
+                  <Stack spacing="6">
+                    <Heading
+                      textAlign="center"
+                      size="xl"
+                      fontWeight="extrabold"
+                    >
+                      Update Project
+                    </Heading>
+                    <FormControl id="title">
+                      <FormLabel>Title</FormLabel>
+                      <Input
+                        name="title"
+                        type="text"
+                        value={values.title}
+                        onChange={onChange}
+                        required
+                      />
+                    </FormControl>
+                    <FormControl id="description">
+                      <FormLabel>Description</FormLabel>
+                      <Textarea
+                        name="description"
+                        value={values.description}
+                        onChange={onChange}
+                      />
+                    </FormControl>
+                    <Button
+                      type="submit"
+                      colorScheme="green"
+                      size="lg"
+                      fontSize="md"
+                      isLoading={editLoading}
+                      loadingText="Updating..."
+                      spinnerPlacement="end"
+                    >
+                      Update Project
+                    </Button>
+                  </Stack>
+                </chakra.form>
+              </ModalBody>
+            </ModalContent>
+          </Modal>
+        </Flex>
+        <Text mt={6} py={2} fontSize="xl" fontWeight="black">
+          description:
+        </Text>
+        <Text fontStyle="italic">"{project.description}"</Text>
+        <Divider py={3} />
+        <Flex pt={1} alignItems="end" w="full" justify="space-between">
+          <VStack alignItems="start">
+            <Text fontStyle="italic" fontWeight="bold">
+              created by:
+            </Text>
+            <VStack justifyContent="center">
+              <Link isExternal as={RouterLink} to={`/user/${project.owner.id}`}>
+                <Avatar size="xl" src={project.owner.avatar} mb={1} />
+              </Link>
+            </VStack>
+            <Text fontSize="md">{project.owner.name}</Text>
+            <Text fontSize="sm">
+              created on{' '}
+              {
+                new Date(project.createdAt)
+                  .toLocaleString('en-GB')
+                  .split(',')[0]
+              }
+            </Text>
           </VStack>
-          <Text fontSize="md">{project.owner.name}</Text>
-          <Text fontSize="sm">
-            created on{' '}
-            {new Date(project.createdAt).toLocaleString('en-GB').split(',')[0]}
-          </Text>
-        </VStack>
-        <Spacer />
-        {authContext.profile.projectOwner ? (
-          <Button colorScheme="red" onClick={onOpen}>
-            Close Project
-          </Button>
-        ) : (
-          <Box></Box>
-        )}
-        <AlertDialog
-          motionPreset="slideInBottom"
-          leastDestructiveRef={cancelRef}
-          onClose={onClose}
-          isOpen={isOpen}
-          isCentered
-        >
-          <AlertDialogOverlay />
+          <Spacer />
+          {authContext.profile.projectOwner ? (
+            <Button colorScheme="red" onClick={onOpen}>
+              Close Project
+            </Button>
+          ) : (
+            <Box></Box>
+          )}
+          <AlertDialog
+            motionPreset="slideInBottom"
+            leastDestructiveRef={cancelRef}
+            onClose={onClose}
+            isOpen={isOpen}
+            isCentered
+          >
+            <AlertDialogOverlay />
 
-          <AlertDialogContent>
-            <AlertDialogHeader>Close Project?</AlertDialogHeader>
-            <AlertDialogCloseButton />
-            <AlertDialogBody>
-              Are you sure you want to close your project? This action can not
-              be undone.
-            </AlertDialogBody>
-            <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={onClose}>
-                No
-              </Button>
-              <Button
-                colorScheme="red"
-                size="lg"
-                fontSize="md"
-                isLoading={closeLoading}
-                loadingText="Closing..."
-                spinnerPlacement="end"
-                onClick={() => {
-                  closeProject();
-                  onClose();
-                }}
-              >
-                Yes
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-      </Flex>
+            <AlertDialogContent>
+              <AlertDialogHeader>Close Project?</AlertDialogHeader>
+              <AlertDialogCloseButton />
+              <AlertDialogBody>
+                Are you sure you want to close your project? This action can not
+                be undone.
+              </AlertDialogBody>
+              <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={onClose}>
+                  No
+                </Button>
+                <Button
+                  colorScheme="red"
+                  size="lg"
+                  fontSize="md"
+                  isLoading={closeLoading}
+                  loadingText="Closing..."
+                  spinnerPlacement="end"
+                  onClick={() => {
+                    closeProject();
+                    onClose();
+                  }}
+                >
+                  Yes
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </Flex>
+      </Box>
     </Box>
   );
 };
